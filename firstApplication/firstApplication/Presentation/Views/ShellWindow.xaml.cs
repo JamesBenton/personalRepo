3b6604793597fb6786969edd6f552dc849dc1eef
jamesbentontamu@gmail.com <jamesbentontamu@gmail.com>
2018-06-21 23:26:00 -0500

    Added the files for my first C# project.

---
+++ firstApplication/firstApplication/Presentation/Views/ShellWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.Composition;
+using System.Windows;
+using firstApplication.Applications.Views;
+
+namespace firstApplication.Presentation.Views
+{
+    [Export(typeof(IShellView))]
+    public partial class ShellWindow : Window, IShellView
+    {
+        public ShellWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ firstApplication/firstApplication/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("firstApplication")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("firstApplication")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
